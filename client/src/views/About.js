5dc8e22a6756bb17a57ec4b9697ce06663cf1b48
Bali Tok <bali.tok@hotmail.com>
2024-04-09 20:51:12 +0800

    Added navbar and footer to about and events page

--- client/src/views/About.js
+++ client/src/views/About.js
@@ -1,10 +1,16 @@
 import React from "react";
+import NavBar from "../components/NavBar";
+import Footer from "../components/Footer";
 
 const About = () => {
   return (
-    <div>
-      <h1 className="text-4xl">About</h1>
-    </div>
+    <React.Fragment>
+      <NavBar />
+      <div>
+        <h1 className="text-4xl">About</h1>
+      </div>
+      <Footer />
+    </React.Fragment>
   );
 };
 
--- client/src/views/Events.js
+++ client/src/views/Events.js
@@ -1,10 +1,16 @@
 import React from "react";
+import Footer from "../components/Footer";
+import NavBar from "../components/NavBar";
 
 const Events = () => {
   return (
-    <div>
-      <h1 className="text-4xl">Events</h1>
-    </div>
+    <React.Fragment>
+      <NavBar />
+      <div>
+        <h1 className="text-4xl">Events</h1>
+      </div>
+      <Footer />
+    </React.Fragment>
   );
 };
 
